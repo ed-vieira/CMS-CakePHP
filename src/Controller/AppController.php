11410353d0b737910d21a85756aa4ea1b1f90042
ed-vieira <edfelip@gmail.com>
2020-04-04 20:33:12 -0300

    auth fuctinality

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -41,15 +41,38 @@ class AppController extends Controller
     {
         parent::initialize();
 
-        $this->loadComponent('RequestHandler', [
-            'enableBeforeRedirect' => false,
-        ]);
-        $this->loadComponent('Flash');
+          $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            // If unauthorized, return them to page they were just on
+            'unauthorizedRedirect' => $this->referer()
+         ]);
 
-        /*
-         * Enable the following component for recommended CakePHP security settings.
-         * see https://book.cakephp.org/3.0/en/controllers/components/security.html
-         */
-        //$this->loadComponent('Security');
+         // Allow the display action so our PagesController
+         // continues to work. Also enable the read only actions.
+        // $this->Auth->allow(['display', 'view', 'index']);
+
+
+         $this->loadComponent('RequestHandler', [
+            'enableBeforeRedirect' => false,
+         ]);
+
+         $this->loadComponent('Flash');
+
+         /*
+          * Enable the following component for recommended CakePHP security settings.
+          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
+          */
+         //$this->loadComponent('Security');
     }
 }
